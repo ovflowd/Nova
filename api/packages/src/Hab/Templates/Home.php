6ff856b985be584b02247f30e368778a05e13c51
Claudio Santoro <claudio.santoro@ieee.org>
2016-11-19 15:14:44 -0200

    - Simplified Home.php Template

--- api/packages/src/Hab/Templates/Home.php
+++ api/packages/src/Hab/Templates/Home.php
@@ -18,18 +18,6 @@ final class Home extends Base
      */
     public function __construct()
     {
-        $stringBuilder = HabTemplate::includeVendor('Home.php');
-
-        //$stringBuilder = '';
-
-        //$stringBuilder .= '<h1>Welcome to the HabClient</h1>';
-
-        //$stringBuilder .= '<br>';
-
-        //$externalUri = HabUtils::generateExternal();
-
-        //$stringBuilder .= "<a href='{$externalUri}'>Enter in Client</a>";
-
-        $this->setResponse($stringBuilder);
+        $this->setResponse(HabTemplate::includeVendor('Home.php'));
     }
 }
